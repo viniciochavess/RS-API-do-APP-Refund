ac441689cdf6c1943a5a551c78b543f13dce3302
viniciochaves <viniciochavess@gmail.com>
2025-06-23 23:12:16 -0300

    task: configuração do prisma e schema

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
-import 'express-async-errors'
+import "express-async-errors";
+import cors from "cors";
+
 export const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (request, response) => {
